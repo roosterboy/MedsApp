730280fc24ae6e0013a38c71aceae3ac8c02b6dd
roosterboy <pat_wynne@roosterville.net>
2021-11-09 20:27:56 -0800

    Initial Commit

---
+++ MedsApp/MedsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsAppApp.swift
+//  MedsApp
+//
+//  Created by Patrick Wynne on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MedsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
